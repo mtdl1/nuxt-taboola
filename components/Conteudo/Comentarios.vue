d7b8de4496a09fbaecb081aee6d3286d435e082d
Vitabe Cosméticos <guilherme@vitabe.com.br>
2023-02-22 17:49:52 -0300

    com gtag

--- components/Conteudo/Comentarios.vue
+++ components/Conteudo/Comentarios.vue
@@ -3,7 +3,6 @@
     <div class="w-11/12 sm:w-2/5 mx-auto">
       <h2 class="text-white">Comentário clientes 100PESO</h2>
       <p class="text-white font-bold mb-5">Confira a opinião de clientes que já experimentaram os <br> benefícios do 100PESO</p>
-      
       <div class="flex justify-around flex-wrap">
         <img src="~/assets/images/content/100PESO-Comentarios-1.webp" loading="lazy" alt="imagem 1" class="mb-3">
         <img src="~/assets/images/content/100PESO-Comentarios-2.webp" loading="lazy" alt="imagem 1" class="mb-3">
@@ -12,7 +11,6 @@
         <img src="~/assets/images/content/100PESO-Comentarios-5.webp" loading="lazy" alt="imagem 1" class="mb-3">
         <img src="~/assets/images/content/100PESO-Comentarios-6.webp" loading="lazy" alt="imagem 1" class="mb-3">
       </div>
-
     </div>
   </div>
 </template>
